17382bade5691987e70287dba88b604b7e71e35d
Ian Salt <ian@isalt.digital>
2017-10-19 12:22:55 +0100

    basic button

---
+++ auth/src/components/reusable/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, TouchOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  return (
+    <TouchOpacity
+      onPress={onPress}
+      style={buttonStyle}
+    >
+      <Text>
+        {children}
+      </Text>
+    </TouchOpacity>
+  );
+};
+
+const styles = {
+  buttonStyle: {
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderColor: '#007aff',
+    borderRadius: 5,
+    borderWidth: 1,
+    flex: 1,
+    marginLeft: 5,
+    marginRight: 5
+  },
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+};
+
+export default Button;
