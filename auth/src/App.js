269f3e765e62e63117a9923e720206151a9b2eee
Ian Salt <ian@isalt.digital>
2017-10-23 12:09:48 +0100

    logging with firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/reusable';
+import { Header, Button, Spinner } from './components/reusable';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBw96h-RX0EF769ywmwuy3hJdrD7CmGtQo',
@@ -16,15 +18,34 @@ class App extends Component {
     });
 
     firebase.auth().onAuthStateChanged((user) => {
-      
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
     });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}> 
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Aye aye Auth!!' />
-        <LoginForm />
+      {this.renderContent()}
       </View>
     );
   }
