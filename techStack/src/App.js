2fd45c533336c91673eb8ec9520215e0cb36e983
Ian Salt <ian@isalt.digital>
2017-10-24 17:10:55 +0100

    connect implemented in Redux list

--- techStack/src/App.js
+++ techStack/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <View>
         <Header headerText='Tech Stack App' />
         <Text>This is my app!</Text>
+        <LibraryList />
       </View>
     </Provider>
   );
--- techStack/src/components/LibraryList.js
+++ techStack/src/components/LibraryList.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import connect from 'react-redux';
+import { connect } from 'react-redux';
 
 class LibraryList extends Component {
   render() {
+    console.log(this.props);  // returns the array
     return;
   }
 }
 
-export default connect()(LibraryList);
+
+//  function takes global state object, the application state from inside
+//  redux store, map it and take some properties off the state object and
+//  provide them as props to the library list component
+
+const mapStateToProps = state => {
+  return { stateDataAsProp: state.libraries };
+};
+
+//  call the function connect() when connect is called it calls another
+//  function, we call that function with the library list
+export default connect(mapStateToProps)(LibraryList);
