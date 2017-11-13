c94bae28c74dbe21af9b4655433c5bdabacd9168
Ian Salt <ian@isalt.digital>
2017-11-13 12:06:28 +0000

    employee creation form basic schafolding

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,8 +1,9 @@
 //  all screens/scenes a user can navigate to
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 // pretty big bug took me a white to figure out, must have a
 // root scene - key=root - to contain all the other scenes
@@ -25,11 +26,17 @@ const RouterComponent = () => {
             //  do not want the user to navigate back
             type='reset'
             //  end hack
-            onRight={() => console.log('right!')}
-            rightTitle='Add Employee'
+            onRight={() => Actions.employeeCreate()}
+            rightTitle='Add'
             key='employeeList'
             component={EmployeeList}
             title='Employees'
+          />
+
+          <Scene
+            key='employeeCreate'
+            component={EmployeeCreate}
+            title='Create Employee'
           />
 
       </Scene>
