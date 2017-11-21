b1b2ae696412497191f91a9ad10d3c6623f62f87
Ian Salt <ian@isalt.digital>
2017-11-21 09:23:54 +0000

    list development

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,11 +1,31 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { ListView, View, Text } from 'react-native';
 import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
   componentWillMount() {
     this.props.employeesFetch();
+    //  call here and below means no matter we
+    //  come from the employee list will be loaded
+    this.createDataSource(this.props);
+  }
+
+  //
+  componentWillReceiveProps(nextProps) {
+    //  nextProps are the next set of props this components
+    //  will be rendered with
+    //  this.props is still the old set of props
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
   }
 
   render() {
@@ -23,5 +43,11 @@ class EmployeeList extends Component {
     );
   }
 }
+
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return { ...val, uid }; // { shift: 'Monday', name: 'S', id: 'dfddfd' }
+  });
+};
 
 export default connect(null, { employeesFetch })(EmployeeList);
