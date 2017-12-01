4ab48158bfe80bcc41e53f0fd109e00be87c4b61
Ian Salt <ian@isalt.digital>
2017-12-01 07:22:02 +0000

    add, edit and delete employee completed

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Container, Section, Button, ConfirmModal } from './common';
 
 class EmployeeEdit extends Component {
   state = { showModal: false };
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value });
@@ -24,6 +25,15 @@ class EmployeeEdit extends Component {
     const { name, phone, shift } = this.props;
 
     Communications.text(phone, `${name}, your upcoming shift is on ${shift}`);
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
+  }
+  onAccept() {
+    const { uid } = this.props.employee;
+
+    this.props.employeeDelete({ uid });
   }
 
   render() {
@@ -54,6 +64,8 @@ class EmployeeEdit extends Component {
 
         <ConfirmModal
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </ConfirmModal>
@@ -70,5 +82,5 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps,
-  { employeeUpdate, employeeSave
+  { employeeUpdate, employeeSave, employeeDelete
   })(EmployeeEdit);
